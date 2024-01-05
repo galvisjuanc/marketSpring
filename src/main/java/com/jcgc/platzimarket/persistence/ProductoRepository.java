fd238d4443bbf515d05bfc3829cefde1efb01efe
Juan Galvis <galvisjuanc@gmail.com>
2024-01-04 23:01:05 -0500

    Add ProductoRepository

---
+++ src/main/java/com/jcgc/platzimarket/persistence/ProductoRepository.java
@@ -0,0 +1,15 @@
+package com.jcgc.platzimarket.persistence;
+
+import com.jcgc.platzimarket.persistence.crud.ProductoCrudRepository;
+import com.jcgc.platzimarket.persistence.entity.Producto;
+
+import java.util.List;
+
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+}
