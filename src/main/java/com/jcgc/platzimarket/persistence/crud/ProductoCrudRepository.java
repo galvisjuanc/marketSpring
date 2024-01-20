9c45f5a2386735ff03981fd9c55732a3cecf1a2c
Juan Galvis <galvisjuanc@gmail.com>
2024-01-20 18:59:08 -0500

    Query Methods part 1

--- src/main/java/com/jcgc/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/jcgc/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,8 @@ package com.jcgc.platzimarket.persistence.crud;
 import com.jcgc.platzimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
 }
