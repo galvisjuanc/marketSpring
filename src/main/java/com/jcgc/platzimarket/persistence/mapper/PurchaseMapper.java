b37648f0e85f89fcbc56d6e620a5fca5b295b29f
Juan Galvis <juan.galvis>
2024-02-20 15:22:25 -0500

    Mappers extra

---
+++ src/main/java/com/jcgc/platzimarket/persistence/mapper/PurchaseMapper.java
@@ -0,0 +1,30 @@
+package com.jcgc.platzimarket.persistence.mapper;
+
+import com.jcgc.platzimarket.domain.Purchase;
+import com.jcgc.platzimarket.persistence.entity.Compra;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {PurchaseMapper.class})
+public interface PurchaseMapper {
+
+    @Mappings({
+            @Mapping(source = "idCompra", target = "purchaseId"),
+            @Mapping(source = "idCliente", target = "clientId"),
+            @Mapping(source = "fecha", target = "date"),
+            @Mapping(source = "medioPago", target = "paymentMethod"),
+            @Mapping(source = "comentario", target = "comment"),
+            @Mapping(source = "estado", target = "state"),
+            @Mapping(source = "productos", target = "items")
+    })
+    Purchase toPurchase(Compra compra);
+    List<Purchase> toPurchase(List<Compra> compras);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "cliente", ignore = true)
+    Compra toCompra(Purchase purchase);
+}
