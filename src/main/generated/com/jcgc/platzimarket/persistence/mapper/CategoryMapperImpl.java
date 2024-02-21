ec996676142fa432ac3d6bae6f969999316dab74
Juan Galvis <juan.galvis>
2024-02-21 18:05:04 -0500

    Purchase Controller

---
+++ src/main/generated/com/jcgc/platzimarket/persistence/mapper/CategoryMapperImpl.java
@@ -0,0 +1,49 @@
+package com.jcgc.platzimarket.persistence.mapper;
+
+import com.jcgc.platzimarket.domain.Category;
+import com.jcgc.platzimarket.persistence.entity.Categoria;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-02-21T17:58:25-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(Categoria categoria) {
+        if ( categoria == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        if ( categoria.getIdCategoria() != null ) {
+            category.setCategoryId( categoria.getIdCategoria() );
+        }
+        category.setCategory( categoria.getDescripcion() );
+        if ( categoria.getEstado() != null ) {
+            category.setActive( categoria.getEstado() );
+        }
+
+        return category;
+    }
+
+    @Override
+    public Categoria toCategoria(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Categoria categoria = new Categoria();
+
+        categoria.setIdCategoria( category.getCategoryId() );
+        categoria.setDescripcion( category.getCategory() );
+        categoria.setEstado( category.isActive() );
+
+        return categoria;
+    }
+}
---
+++ src/main/generated/com/jcgc/platzimarket/persistence/mapper/ProductMapperImpl.java
@@ -0,0 +1,83 @@
+package com.jcgc.platzimarket.persistence.mapper;
+
+import com.jcgc.platzimarket.domain.Product;
+import com.jcgc.platzimarket.persistence.entity.Producto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-02-21T17:58:25-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
+)
+@Component
+public class ProductMapperImpl implements ProductMapper {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public Product toProduct(Producto producto) {
+        if ( producto == null ) {
+            return null;
+        }
+
+        Product product = new Product();
+
+        if ( producto.getIdProducto() != null ) {
+            product.setProductId( producto.getIdProducto() );
+        }
+        product.setName( producto.getNombre() );
+        if ( producto.getIdCategoria() != null ) {
+            product.setCategoryId( producto.getIdCategoria() );
+        }
+        if ( producto.getPrecioVenta() != null ) {
+            product.setPrice( producto.getPrecioVenta() );
+        }
+        if ( producto.getCantidadStock() != null ) {
+            product.setStock( producto.getCantidadStock() );
+        }
+        if ( producto.getEstado() != null ) {
+            product.setActive( producto.getEstado() );
+        }
+        product.setCategory( categoryMapper.toCategory( producto.getCategoria() ) );
+
+        return product;
+    }
+
+    @Override
+    public List<Product> toProducts(List<Producto> product) {
+        if ( product == null ) {
+            return null;
+        }
+
+        List<Product> list = new ArrayList<Product>( product.size() );
+        for ( Producto producto : product ) {
+            list.add( toProduct( producto ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Producto toProducto(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+
+        Producto producto = new Producto();
+
+        producto.setIdProducto( product.getProductId() );
+        producto.setNombre( product.getName() );
+        producto.setIdCategoria( product.getCategoryId() );
+        producto.setPrecioVenta( product.getPrice() );
+        producto.setCantidadStock( product.getStock() );
+        producto.setEstado( product.isActive() );
+        producto.setCategoria( categoryMapper.toCategoria( product.getCategory() ) );
+
+        return producto;
+    }
+}
