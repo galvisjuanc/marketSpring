0ee31d6b0a672e561afea6aad4a0b0fd131029cf
Juan Galvis <juan.galvis>
2024-02-26 17:10:11 -0500

    Using Operation, Api Response y Parameter

--- src/main/java/com/jcgc/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/jcgc/platzimarket/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.jcgc.platzimarket.web.controller;
 
 import com.jcgc.platzimarket.domain.Product;
 import com.jcgc.platzimarket.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +21,18 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @Operation(summary = "Get all supermarket products")
+    @ApiResponse(responseCode = "200", description = "OK")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @Operation(summary = "Search a product with an ID")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "404", description = "product not found")
+    public ResponseEntity<Product> getProduct(@Parameter(description = "The ID of the product", required = true, example = "7")
+                                                  @PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
